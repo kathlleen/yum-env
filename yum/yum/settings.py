42de5bf7b36a97e2acf78410bc49d6c84ac33f25
kathlleen <katrina2005by@gmail.com>
2024-10-03 12:04:26 +0300

    Create PostgreSql db, load cuisines from fixtures

--- yum/yum/settings.py
+++ yum/yum/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'yum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'yum',
+        'USER': 'yum',
+        'PASSWORD': 'yumpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
