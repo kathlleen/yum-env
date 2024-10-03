81609d97a0b7b0cef1044aa10b2d154ad28988e9
kathlleen <katrina2005by@gmail.com>
2024-10-03 11:16:15 +0300

    Cuisine model, fixtures

--- yum/restaurans/admin.py
+++ yum/restaurans/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from restaurans.models import Cuisine
+
 # Register your models here.
+# admin.site.register(Cuisine)
+
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('name',)}
--- yum/restaurans/models.py
+++ yum/restaurans/models.py
@@ -1,3 +1,32 @@
+# from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+#
+# # Create your models here.
+# class Restaurans(AbstractUser):
+#     name = models.CharField(max_length=60, verbose_name="Название")
+#     slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, verbose_name='URL')
+#     address = models.TextField(null=True, blank=True, verbose_name="Адрес")
+#     phone_number = models.CharField(max_length=20, verbose_name="Контактный телефон")
+#
+#
+#     class Meta:
+#         db_table = 'restaurans'
+#         verbose_name = 'Ресторан'
+#         verbose_name_plural = 'Рестораны'
+#
+#     def __str__(self):
+#         return f'{self.name}'
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=60, verbose_name="Название")
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'cuisine'
+        verbose_name = 'Кухня'
+        verbose_name_plural = 'Кухни'
+
+    def __str__(self):
+        return f'{self.name}'
