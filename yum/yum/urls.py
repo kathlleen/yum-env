3187b15a24ea19de0419948b631a4fc59e4d66eb
kathlleen <katrina2005by@gmail.com>
2024-10-05 14:37:23 +0300

    Categories and dishes models

--- yum/yum/urls.py
+++ yum/yum/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('user/', include('users.urls', namespace='users')),
-
+    path('menu/', include('menu.urls', namespace='menu')),
 ]
