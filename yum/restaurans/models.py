ce2e342c44660078add4e7cb26fa7c3e0cb293eb
kathlleen <katrina2005by@gmail.com>
2024-10-21 18:26:00 +0300

    Added cuisine for restaurans

--- yum/restaurans/models.py
+++ yum/restaurans/models.py
@@ -4,22 +4,6 @@ from django.db import models
 
 
 # Create your models here.
-class Restaurans(models.Model):
-    name = models.CharField(max_length=60, verbose_name="Название")
-    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, verbose_name='URL')
-    address = models.TextField(null=True, blank=True, verbose_name="Адрес")
-    description = models.TextField(null=True, blank=True, verbose_name="Описание")
-    phone_number = models.CharField(null=True, blank=True, max_length=20, verbose_name="Контактный телефон")
-    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Изображение')
-    owner = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE, related_name='restaurant', verbose_name='Админ')
-
-    class Meta:
-        db_table = 'restaurans'
-        verbose_name = 'Ресторан'
-        verbose_name_plural = 'Рестораны'
-
-    def __str__(self):
-        return f'{self.name}'
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=60, verbose_name="Название")
@@ -32,3 +16,23 @@ class Cuisine(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+class Restaurans(models.Model):
+    name = models.CharField(max_length=60, verbose_name="Название")
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, verbose_name='URL')
+    address = models.TextField(null=True, blank=True, verbose_name="Адрес")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+    phone_number = models.CharField(null=True, blank=True, max_length=20, verbose_name="Контактный телефон")
+    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Изображение')
+    owner = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE, related_name='restaurant', verbose_name='Админ')
+    cuisine = models.ForeignKey(to=Cuisine, blank=True, on_delete=models.SET_NULL, null=True, verbose_name="Кухня")
+
+
+    class Meta:
+        db_table = 'restaurans'
+        verbose_name = 'Ресторан'
+        verbose_name_plural = 'Рестораны'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
