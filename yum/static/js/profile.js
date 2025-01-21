cfdb5597ab5d361462c383a368154c89b8bc437d
kathlleen <katrina2005by@gmail.com>
2025-01-21 16:56:49 +0300

    Profile edits

--- yum/static/js/profile.js
+++ yum/static/js/profile.js
@@ -1,27 +1,41 @@
 // scripts.js
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const toggleButtons = document.querySelectorAll('.details-btn');
+    let isProcessing = false;
 
     toggleButtons.forEach(button => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
+            if (isProcessing) return; // Пропускаем, если событие уже обрабатывается
+                isProcessing = true;
             const orderDetails = this.closest('.order').querySelector('.order-details');
-            orderDetails.style.display = orderDetails.style.display === 'none' || orderDetails.style.display === '' ? 'block' : 'none';
+
+            // Переключаем класс "open" для добавления/удаления анимации
+            orderDetails.classList.toggle('open');
+            setTimeout(() => {
+                    isProcessing = false;
+                }, 300);
         });
     });
 });
 
 
 document.addEventListener("DOMContentLoaded", function() {
+        let isProcessing = false;
         const editButton = document.querySelector(".user-info .btn");
         const editForm = document.querySelector(".edit-form");
 
         editButton.addEventListener("click", function() {
+            if (isProcessing) return; // Пропускаем, если событие уже обрабатывается
+                isProcessing = true;
+
             if (editForm.style.display === "none") {
                 editForm.style.display = "block";
             } else {
                 editForm.style.display = "none";
             }
+            setTimeout(() => {
+                    isProcessing = false;
+                }, 300);
         });
     });
 
