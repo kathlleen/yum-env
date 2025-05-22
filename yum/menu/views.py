77cda9afad74871a93859b58a062f162c490e8ef
kathlleen <katrina2005by@gmail.com>
2025-05-22 13:24:06 +0300

    Labels in menu

--- yum/menu/views.py
+++ yum/menu/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import DetailView, TemplateView
 from django.shortcuts import get_object_or_404
 from restaurans.models import Restaurans
-from menu.models import Dish, Categories
+from menu.models import Dish, Categories, LabelPreference
+
 
 class MenuView(TemplateView):
     template_name = 'menu/restaurant_menu.html'
@@ -16,7 +17,7 @@ class MenuView(TemplateView):
         context['title'] = restaurant.name
 
         # Получаем блюда и категории
-        dishes = Dish.objects.filter(restaurant=restaurant)
+        dishes = Dish.objects.filter(restaurant=restaurant).prefetch_related('labels')
         categories = Categories.objects.filter(dish__in=dishes).distinct()
 
         # Сортируем блюда по категориям
@@ -26,6 +27,21 @@ class MenuView(TemplateView):
 
         context['categories'] = categories
         context['dishes_by_category'] = dishes_by_category
+
+        # Если пользователь авторизован — добавим его предпочтения
+        user = self.request.user
+        if user.is_authenticated:
+            context['liked_ingredients'] = set(map(str.strip, (user.liked_ingredients or '').lower().split(',')))
+            context['disliked_ingredients'] = set(map(str.strip, (user.disliked_ingredients or '').lower().split(',')))
+
+            preferences = LabelPreference.objects.filter(user=user).select_related('label')
+            context['liked_labels'] = set(p.label for p in preferences if p.preference_type == 'like')
+            context['disliked_labels'] = set(p.label for p in preferences if p.preference_type == 'dislike')
+        else:
+            context['liked_ingredients'] = set()
+            context['disliked_ingredients'] = set()
+            context['liked_labels'] = set()
+            context['disliked_labels'] = set()
 
         return context
 
